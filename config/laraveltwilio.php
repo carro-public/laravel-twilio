0102bad4d4c5c43874c34db94258e549b8db7b2c
Set Kyar Wa Lar (Universe) <setkyar16@gmail.com>
2018-09-12 12:44:17 +0800

    implment sms wrapper

--- config/laraveltwilio.php
+++ config/laraveltwilio.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-    //
+    'account_sid' => env('TWILIO_ACCOUNT_SID'),
+    
+    'auth_token' => env('TWILIO_AUTH_TOKEN'),
+
+    'from' => env('TWILIO_FROM')
 ];
--- src/LaravelTwilio.php
+++ src/LaravelTwilio.php
@@ -2,7 +2,29 @@
 
 namespace CarroPublic\LaravelTwilio;
 
+use Twilio\Rest\Client;
+
 class LaravelTwilio
 {
-    // Build wonderful things
+    protected $client;
+    
+    public function __construct(){
+        $sid = config('laraveltwilio.account_sid');
+        $token = config('laraveltwilio.auth_token');
+
+        $this->client = new Client($sid, $token); 
+    }
+
+    public function sendSMS($to, $message, $from=null){
+
+        $message = $this->client->messages->create(
+            $to,
+            [
+                'from' => isset($from) ? $from : config('laraveltwilio.from'),
+                'body' => $message
+            ]
+        );
+
+        return $message->sid;
+    }
 }
